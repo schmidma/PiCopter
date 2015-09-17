6ecac06683fceed18a816595356ebcb1528b3bba
raspberry-mac <raspberry@Technik-AGs-Mac-mini.local>
2015-09-17 16:55:49 +0200

    [BUGFIX] Whitespaces entfernt

--- accel.py
+++ accel.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 ##########################################
-##	gyro.py - Gyroskop-Klasse	##
+gyro.py - Gyroskop-Klasse
 ##########################################
 
 #IMPORT
@@ -11,28 +11,28 @@ import smbus
 import string
 
 class Accel (object):
+
 	def __init__(self, i2c_bus = 1, i2c_address = 0x53):
 		self.__i2c_bus = smbus.SMBus(i2c_bus)
 		self.i2c_address = i2c_address
-		
 		self.config()
-		pass
-	
+		
 	def config(self):
 		#enable the accelerometer
 		self.__i2c_bus.write_byte_data(i2c_address,0x2D,0x08)
-
+		
 	def readList(self):
-        return self.__i2c_bus.read_i2c_block_data(self.i2c_address, 0x32, 6)
-    
-    def read(self):
-        raw = self.readList()
-        res = []
-        for i in range(0, 6, 2):
-            g = raw[i] | (raw[i+1] << 8)
-            if g > 32767: g -= 65536
-            res.append(g)
-        return res
+		result = self.__i2c_bus.read_i2c_block_data(self.i2c_address, 0x32, 6)
+		return result
+
+	def read(self):
+		raw = self.readList()
+		res = []
+		for i in range(0, 6, 2):
+			g = raw[i] | (raw[i+1] << 8)
+			if g > 32767: g -= 65536
+			res.append(g)
+		return res
 
 	def getX(self):
 		X = self.readX()
@@ -48,6 +48,6 @@ class Accel (object):
 		Y = self.readY()
 		Y = self.getSignedNumber(Y)
 		return Y
-    
+
 accel = Accel()
 print accel.read()
