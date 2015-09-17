6cc61ad19fc7707f4b660ede8116346f8bb42209
raspberry-mac <raspberry@Technik-AGs-Mac-mini.local>
2015-09-17 17:13:19 +0200

    [REFACTOR] Allgemeines Code-Refactoring; Methoden strukturiert und aufgeräumt; Debug-Objekt-Erstellung entfernt

--- accel.py
+++ accel.py
@@ -15,39 +15,30 @@ class Accel (object):
 	def __init__(self, i2c_bus = 1, i2c_address = 0x53):
 		self.__i2c_bus = smbus.SMBus(i2c_bus)
 		self.i2c_address = i2c_address
-		self.config()
-		
-	def config(self):
-		#enable the accelerometer
-		self.__i2c_bus.write_byte_data(self.i2c_address,0x2D,0x08)
-		
-	def readList(self):
-		result = self.__i2c_bus.read_i2c_block_data(self.i2c_address, 0x32, 6)
-		return result
+		self.__i2c_bus.write_byte_data(self.i2c_address, 0x2D, 0x08)
 
 	def read(self):
-		raw = self.readList()
+		self.bytes = self.__i2c_bus.read_i2c_block_data(self.i2c_address, 0x32, 6)
+	
+	def getResult(self, refresh=0):
+		if refresh:
+			self.read()
 		res = []
 		for i in range(0, 6, 2):
-			g = raw[i] | (raw[i+1] << 8)
-			if g > 32767: g -= 65536
+			g = self.bytes[i] | (self.bytes[i+1] << 8)
+			if g > 32767:
+				g -= 65536
 			res.append(g)
 		return res
 
 	def getX(self):
-		X = self.readX()
-		X = self.getSignedNumber(X)
+		X = self.read()[0]
 		return X
 	
 	def getY(self):
-		Y = self.readY()
-		Y = self.getSignedNumber(Y)
+		Y = self.read()[1]
 		return Y
 	
 	def getZ(self):
-		Y = self.readY()
-		Y = self.getSignedNumber(Y)
+		Y = self.read()[2]
 		return Y
-
-accel = Accel()
-print accel.read()
