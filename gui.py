49551540cda42d2d9f1e121bc32f38f9f49da90a
Maximilian Schmidt <maximilian@schmidt.so>
2015-11-22 16:27:55 +0100

    [DEBUG] Variablenverweis korrigiert (self.gui -> self.)

--- gui.py
+++ gui.py
@@ -51,12 +51,12 @@ class Gui():
                 i.bkgd(curses.color_pair(1))
                 
     def showMessage(self, message):
-        self.gui.win_message.addstr(0, 0, str(message), curses.A_BLINK)
-        self.gui.win_message.refresh()
+        self.win_message.addstr(0, 0, str(message), curses.A_BLINK)
+        self.win_message.refresh()
         
     def hideMessage(self):
-        self.gui.win_message.clear()
-        self.gui.win_message.refresh()
+        self.win_message.clear()
+        self.win_message.refresh()
     
     def guiTick(self, fps, throttle, isStart, isHoldHeight, isHoldPosition, isAccel):
         self.win_fps.addstr(1, 1, str(int(fps)))
