3e8e05b36e0ca52e15139cba70a8bca69afc00cc
Maximilian Schmidt <schmidma@katharineum.de>
2015-11-24 14:42:24 +0100

    Button-ID-Variablen durch ein Dictionary ersetzt

--- gamepad.py
+++ gamepad.py
@@ -23,56 +23,37 @@ class Gamepad():
         self.throttle = 0
         self.axis = [0,0,0]
         
-        self.bCalibFront = 4
-        self.bCalibRear = 6
-        self.bCalibRight =5
-        self.bCalibLeft = 7
-        self.bCalibRotLeft = 10
-        self.bCalibRotRight = 11
-        self.bCalib = 0
-        self.aXAxis = 0
-        self.aYAxis = 1
-        
-        self.bStart = 3
-        self.bHome = 16
-        self.bHoldHeight = 12
-        self.bHoldPosition = 13
-        self.bAccel = 15
-        self.aThrottle = 13
+        self.mapping = {"select":0, "start":3, "up":4, "right":5, "down":6, "left":7, "l2":10, "r2":13, "r1":11, "l1":10, "triangle":12, "circle":13, "x":14, "square":15, "PS":16, "l-stick-x":0, "l-stick-y":1, "r-stick-x":2, "r-stick-y":3}
     
     def handleButtonDown(self, button):
-        if button == self.bStart:
+        if button == self.mapping["start"]:
             self.toggleStart()
-        elif button == self.bHoldHeight:
+        elif button == self.mapping["triangle"]:
             self.toggleHoldHeight()
-            #print("holdHeight: "+str(self.isHoldHeight))
-        elif button == self.bHoldPosition:
+        elif button == self.mapping["circle"]:
             self.toggleHoldPosition()
-            #print("holdPosition: "+str(self.isHoldPosition))
-        elif button == self.bCalib:
+        elif button == self.mapping["select"]:
             self.toggleCalib()
-            #print("calib: "+str(self.isCalib))
-        elif button == self.bHome:
+        elif button == self.mapping["PS"]:
             pygame.event.post(pygame.event.Event(pygame.QUIT))
-        elif button == self.bAccel:
+        elif button == self.mapping["square"]:
             self.toggleAccel()
-        elif button == self.bCalibFront:
+        elif button == self.mapping["up"]:
             self.calibrationValues[0] += 1
-        elif button == self.bCalibRear:
+        elif button == self.mapping["down"]:
             self.calibrationValues[0] -= 1
-        elif button == self.bCalibRight:
+        elif button == self.mapping["right"]:
             self.calibrationValues[1] += 1
-        elif button == self.bCalibLeft:
+        elif button == self.mapping["left"]:
             self.calibrationValues[1] -= 1
-        elif button == self.bCalibRotLeft:
+        elif button == self.mapping["l1"]:
             self.calibrationValues[2] += 1
-        elif button == self.bCalibRotRight:
+        elif button == self.mapping["r1"]:
             self.calibrationValues[2] -= 1
     
     def handleButtonUp(self, button):
-        if button == self.bCalib:
+        if button == self.mapping["select"]:
             self.toggleCalib()
-            #print("calib: "+str(self.isCalib))
     
     def toggleAccel(self):
         self.isAccel = not self.isAccel
@@ -90,9 +71,9 @@ class Gamepad():
         self.isCalib = not self.isCalib
     
     def getThrottle(self):
-        self.throttle = (self.pad.get_axis(self.aThrottle)+1)/2*100
+        self.throttle = (self.pad.get_axis(self.mapping["r2"])+1)/2*100
         return self.throttle
     
     def getAxis(self):
-        self.axis = [-self.pad.get_axis(self.aYAxis),self.pad.get_axis(self.aXAxis),0]
+        self.axis = [-self.pad.get_axis(self.mapping["l-stick-y"]),self.pad.get_axis(self.mapping["l-stick-x"),0]
         return self.axis
