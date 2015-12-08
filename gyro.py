9632cfc9385c1d1e8dc72ec7de154cd100938964
Maximilian Schmidt <maximilian@schmidt.so>
2015-12-08 13:41:22 +0100

    [REWORK] __CalibrationIteration ausgelagert [REWORK] Variablen pitch und roll eingeführt [REWORK] CalibrationValues in Offset geändert

--- gyro.py
+++ gyro.py
@@ -8,6 +8,9 @@
 import smbus
 
 class Gyro():
+    
+    __CalibrationIteration = 10
+    
     def __init__(self, i2c_bus = 1, i2c_address = 0x69):
         self.__i2c_bus = smbus.SMBus(i2c_bus)
         
@@ -16,10 +19,12 @@ class Gyro():
         self.__i2c_bus.write_byte_data(self.i2c_address, 0x20, 0x0F)
         self.__i2c_bus.write_byte_data(self.i2c_address, 0x23, 0b00110000)
         
-        self.calibrationValues = [0,0,0]
+        self.offset = [0,0,0]
         
-        self.gyro_diff = [0,0,0]
         self.gyro_axes = [0,0,0]
+        
+        self.pitch = 0
+        self.roll = 0
 
     def read(self):
         self.__i2c_bus.write_byte(self.i2c_address,0x28)
@@ -46,46 +51,22 @@ class Gyro():
         
         for i in range(0, 6, 2):
             g = self.bytes[i] | (self.bytes[i+1] << 8)
-    
             if g > 32767:
                 g -= 65536
-    
             res.append(g)
         
-        res[1] = -res[1]
-        
-        """for i in range(3):
-            if 0 < res[i] < 10:
-                res[i] = 0
-            elif 0 > res[i] > -10:
-                res[i] = 0
-        """
         return res
     
     def gyroCalculation(self, delta_time):
         result = self.getResult()
-        print(result)
-        print(self.calibrationValues)
-        print(delta_time)
         
-        self.gyro_axes = [self.gyro_axes[i]+((result[i]+self.calibrationValues[i])/10*(delta_time/1000.0)) for i in range (3)]
-        print(self.gyro_axes)
-        self.gyro_diff = [self.gyro_axes[i]/100 for i in range(3)]
-
-        for i in range(3):
-            if self.gyro_diff[i] > 10:
-                self.gyro_diff[i] = 10
-            elif self.gyro_diff[i] < -10:
-                self.gyro_diff[i] = -10
+        self.gyro_axes = [self.gyro_axes[i]+((result[i]-self.offset[i])/10*(delta_time/1000.0)) for i in range (3)]
+        self.pitch = self.gyro_axes[0]
+        self.roll = self.gyro_axes[1]
     
     def calibrateGyro(self):
-        for i in range(10):
+        for i in range(__CalibrationIteration):
             result = self.getResult()
-            self.calibrationValues = [self.calibrationValues[a] + result[a] for a in range(3)]
+            self.offset = [self.offset[a] + result[a] for a in range(3)]
             
-        self.calibrationValues = [-result[0]/10, -result[1]/10, -result[2]/10]
-    
-    def getGyroValues(self, delta_time):
-        gyro_result = self.getResult()
-        self.gyro_axes = [(self.gyro_axes[i] + gyro_result[i]+self.calibrationValues[i])*delta_time for i in range(3)]
-        return self.gyro_axes
+        self.offset = [result[0]/__CalibrationIteration, result[1]/__CalibrationIteration, result[2]/__CalibrationIteration]
